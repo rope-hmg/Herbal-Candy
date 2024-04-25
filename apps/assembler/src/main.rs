3144e7a86d6053344115f6696b414a07eb1951a5
Hector <rope.hmg@proton.me>
2024-04-25 21:09:12 +0100

    The assembler can now load files from disk

--- apps/assembler/src/main.rs
+++ apps/assembler/src/main.rs
@@ -1,32 +1,16 @@
 #![allow(non_camel_case_types)]
 
+use std::env::args;
+
 mod assembler;
 mod lexer;
 mod object;
 mod parser;
 mod token;
 
-static TEST: &str = r#"
-[code]
-    call_i :fibonacci
-    halt
-
-[data]
-    count .i32 10
-
-[code]
-fibonacci:
-    ld_a.32   r0,   &count
-    mov       r2,   one
-loop:
-    s_sub.i32 r0,   r0, one
-    s_add.i32 r3,   r1, r2
-    mov       r1,   r2
-    mov       r2,   r3
-    jnz_i     r0,   :loop
-    ret
-"#;
-
 fn main() {
-    assembler::Assembler::new(TEST).unwrap().assemble();
+    args().skip(1).for_each(|arg| {
+        let source = std::fs::read_to_string(arg).unwrap();
+        let object = assembler::Assembler::new(&source).unwrap().assemble();
+    });
 }
